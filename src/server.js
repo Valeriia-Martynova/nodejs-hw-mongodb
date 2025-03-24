6998525c71f702596c2293ab8e3ecccbc2fcb35f
Valeriia Martynova <vmartynova95@gmail.com>
2025-03-24 21:52:36 +0200

    update server.js

--- src/server.js
+++ src/server.js
@@ -42,11 +42,8 @@ export const setupServer = () => {
 
     res.status(200).json({
       status: 200,
-      message: 'Successfully found contact with id {contactId}!',
-      data: {
-        contact,
-        id: contactId,
-      },
+      message: `Successfully found contact with id ${contactId}!`,
+      data: contact,
     });
   });
 
