a01e79c2d61844b52a05cb6cce970133201fa27c
Valeriia Martynova <vmartynova95@gmail.com>
2025-04-05 20:16:02 +0300

    add homework switch hw4-validation

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -24,7 +24,7 @@ const contactSchema = new Schema(
       default: 'personal',
     },
   },
-  { timestamps: true },
+  { timestamps: true, versionKey: false },
 );
 
 export const ContactsCollection = model('contact', contactSchema);
