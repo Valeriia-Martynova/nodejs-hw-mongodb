226c9631dd5802275c75edc2808d4ff2bf101308
Valeriia Martynova <vmartynova95@gmail.com>
2025-04-10 22:53:49 +0300

    add step 5 hw

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,9 @@
 import bcrypt from 'bcrypt';
+import { randomBytes } from 'crypto';
 import createHttpError from 'http-errors';
 import { UserCollection } from '../db/models/user';
+import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants';
+import { SessionCollection } from '../db/models/session';
 
 export const registerUser = async (payload) => {
   const user = await UserCollection.findOne({ email: payload.email });
@@ -12,5 +15,27 @@ export const registerUser = async (payload) => {
   return await UserCollection.create({
     ...payload,
     password: encryptedPassword,
+  });
+};
+
+export const loginUser = async (payload) => {
+  const user = await UserCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(401, 'User not found!');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized!');
+  }
+  await SessionCollection.deleteOne({ userId: user._id });
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + THIRTY_DAYS),
   });
 };
