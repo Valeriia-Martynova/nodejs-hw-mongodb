bd085a6549ddf1e245b64f03d14663a263f17e1a
Valeriia Martynova <vmartynova95@gmail.com>
2025-05-05 23:14:10 +0300

    add step 5

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', 3000));
 
@@ -33,11 +34,13 @@ export const setupServer = () => {
 
   app.use(router);
 
+  app.use(express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
+
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
-
-  app.use(express.static(UPLOAD_DIR));
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
